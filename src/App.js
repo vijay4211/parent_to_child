1fc7f29aae7d43e103aea0a2b148f4bd85235b6b
vijay4211 <vijayppandit2042.com>
2024-05-30 19:06:43 +0530

    delete some style file

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import "./App.css";
 import "./Style.css";
-import style from "./custome.module.css";
 
 function App(){
   return(
     <div className="App">
-      <h2 className="primary">Style type 1 in React js</h2>
-      <h2 style={{color:"red", backgroundColor:"black"}}>Style type 2 in React js</h2>
-      <h2 className={style.sucess}>Style type 3 in React js</h2>
+     
     </div>
   )
 }
 export default App;
 
-//style 1st bracket use jsx
-//style 2nd bracke use css style
+
