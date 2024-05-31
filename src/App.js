497bc1778975f32419970dbc759a71dcf246676b
vijay4211 <vijayppandit2042.com>
2024-05-31 15:04:52 +0530

    some litel changes

--- src/App.js
+++ src/App.js
@@ -18,13 +18,15 @@ function App() {
           <td>Contact</td>
         </tr>
       {
-        students.map((data)=>(
+        students.map(
+          (data)=>(
           <tr>
           <td>{data.name}</td>
           <td>{data.email}</td>
           <td>{data.contact}</td>
         </tr>
-        ))
+        )
+      )
       }
       </table>
     </div>
