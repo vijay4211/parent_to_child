eab625b9af040aeeaba7a548088e60736dae5e43
vijay4211 <vijayppandit2042.com>
2024-05-31 15:45:43 +0530

    used bootstrap and filter using number form arry of object

--- src/App.js
+++ src/App.js
@@ -1,32 +1,45 @@
 import "./App.css";
+import Table from 'react-bootstrap/Table';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   
- const students = [
+ const users = [
   {name:"anil", email:"anil@gmail.com", contact:12356 }, //row-1
-  {name:"siddhu", email:"siddhu@gmail.com", contact:111222 },  //row-2
-  {name:"peter", email:"peter@gmail.com", contact:3333 } //row-3
+  {name:"siddhu", email:"siddhu@gmail.com", contact:"111" },  //row-2
+  {name:"peter", email:"peter@gmail.com", contact:"111" } //row-3
  ] 
  
 
   return (
     <div className="App">
-      <table border={1}>
+       <h4>List With Bootstrap and Unique Key</h4>
+       <Table striped bordered hover variant="dark">
+      <thead>
         <tr>
-        <td>Name</td>
-          <td>Email</td>
-          <td>Contact</td>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Contact</th>
         </tr>
-      {
-        students.map((data)=>(
-          <tr>
-          <td>{data.name}</td>
-          <td>{data.email}</td>
-          <td>{data.contact}</td>
-        </tr>
-        ))
-      }
-      </table>
+      </thead>
+      <tbody>
+        {
+          users.map(
+            (item, index)=>(
+              item.contact === '111' ?
+              <tr key={index}>
+              <td>{item.name}</td>
+              <td>{item.email}</td>
+              <td>{item.contact}</td>
+              </tr> : null
+            )
+          )
+        }
+        
+      </tbody>
+    </Table>
     </div>
   );
 }
