caa9efc4df17e0d0389b0bda753c12457c3fc745
vijay4211 <vijayppandit2042.com>
2024-06-01 12:57:34 +0530

    Nested array using map

--- src/App.js
+++ src/App.js
@@ -1,45 +1,85 @@
 import "./App.css";
-import Table from 'react-bootstrap/Table';
+import Table from "react-bootstrap/Table";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  
- const users = [
-  {name:"anil", email:"anil@gmail.com", contact:12356 }, //row-1
-  {name:"siddhu", email:"siddhu@gmail.com", contact:"111" },  //row-2
-  {name:"peter", email:"peter@gmail.com", contact:"111" } //row-3
- ] 
- 
+  const users = [
+    {
+      name: "anil",
+      email: "anil@gmail.com",
+      address: [
+        { Hn: "10", city: "Noida", country: "India" },
+        { Hn: "12", city: "Pune", country: "Dubai" },
+        { Hn: "13", city: "Mumbai", country: "Canada" },
+        { Hn: "14", city: "Kolhapur", country: "America" },
+      ],
+    },
+    {
+      name: "siddhu",
+      email: "siddhu@gmail.com",
+      address: [
+        { Hn: "10", city: "Noida", country: "India" },
+        { Hn: "12", city: "Pune", country: "Dubai" },
+        { Hn: "13", city: "Mumbai", country: "Canada" },
+        { Hn: "14", city: "Kolhapur", country: "America" },
+      ],
+    },
+    {
+      name: "peter",
+      email: "peter@gmail.com",
+      address: [
+        { Hn: "10", city: "Noida", country: "India" },
+        { Hn: "12", city: "Pune", country: "Dubai" },
+        { Hn: "13", city: "Mumbai", country: "Canada" },
+        { Hn: "14", city: "Kolhapur", country: "America" },
+      ],
+    },
+  ];
 
   return (
     <div className="App">
-       <h4>List With Bootstrap and Unique Key</h4>
-       <Table striped bordered hover variant="dark">
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Email</th>
-          <th>Contact</th>
-        </tr>
-      </thead>
-      <tbody>
-        {
-          users.map(
-            (item, index)=>(
-              item.contact === '111' ?
-              <tr key={index}>
-              <td>{item.name}</td>
-              <td>{item.email}</td>
-              <td>{item.contact}</td>
-              </tr> : null
-            )
-          )
-        }
-        
-      </tbody>
-    </Table>
+      <h4>ist With Nested Array</h4>
+        <Table variant="dark" striped>
+            <thead>
+                <tr>
+                   <td>S.No</td>
+                   <td>Name</td>
+                   <td>Email</td>
+                   <td>Address</td>
+                </tr>
+            </thead>
+            <tbody>
+              {
+                 users.map(
+                  (item, index)=>(
+                    <tr key={index}>
+                    <td>{index+1}</td>
+                    <td>{item.name}</td>
+                    <td>{item.email}</td>
+                    <td>
+                    <Table variant="dark" striped>
+                      <tbody>
+                      {
+                         item.address.map(
+                          (data, index)=>(
+                             <tr key={index}>
+                               <td>{data.Hn}</td>
+                               <td>{data.city}</td>
+                               <td>{data.country}</td>
+                             </tr>
+                          )
+                         )
+                      }
+                      </tbody>
+                    </Table>
+                    </td>
+                 </tr>
+                  )
+                 )
+              }
+            </tbody>
+        </Table>
     </div>
   );
 }
