eecdb5d01e4a2f8574a672e564c46a98bbe7c930
vijay4211 <vijayppandit2042.com>
2024-06-01 15:38:13 +0530

    add othe logical code and remove older code

--- src/App.js
+++ src/App.js
@@ -4,27 +4,19 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import User from "./User";
 
 function App() {
-  const users = [
-    { name: "Anil", email: "anil@gmail.com", contact: "111"},
-    { name: "Bruce", email: "bruce@gmail.com", contact: "222"},
-    { name: "Peter", email: "peter@gmail.com", contact: "333"},
-    { name: "Sam", email: "sam@gmail.com", contact: "444"},
-  ];
-
-  function handler(){
-    alert("Hello");
+  //function
+  function test() {
+    console.log("test function");
   }
+  test();
 
   return (
     <div className="App">
       <h4>Reuse Component With List</h4>
-      {
-        users.map(
-          (item, index)=>(
-              <User username={item.name} useremail={item.email} usercontact={item.contact} functionhandler={handler}/>
-          )
-        )
-      }
+      <User />
+      <User />
+      <User />
+      <User />
     </div>
   );
 }
--- src/User.js
+++ src/User.js
@@ -1,22 +1,9 @@
 import React from "react";
 
-function User(props) {
-  const styling = {
-    background : "skyblue",
-    marginTop:"40px"
-  }
-  const NameStyle = {
-    background : "pink"
-  }
-  const ButtonSytle = {
-    padding:"5px 10px 10px 5px"
-  }
+function User() {
   return (
-    <div className="App" style={styling}>
-      <h4 style={NameStyle}>Name : {props.username} </h4>
-      <h4>Email : {props.useremail} </h4>
-      <h4>Contact : {props.usercontact}</h4>
-      <button onClick={()=> props.functionhandler()} style={ButtonSytle}>Click</button>
+    <div className="App">
+      <h4>User Component</h4>
     </div>
   );
 }
