bc9855b1505de415b05b8f130333cd747eb5dad3
vijay4211 <vijayppandit2042.com>
2024-06-08 16:20:58 +0530

    useMemo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import { PureComponent } from "react";
+import { useState, useMemo } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import User from "./User";
-class App extends PureComponent {
-  //define constructor method
-  constructor() {
-    super();
-    //define state
-    this.state = {
-      count: 1,
-    };
-  }
 
-  render() {
-    return (
-      <div className="App">
-        <User NumberCount={this.state.count} />
-        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
-          Update Count
-        </button>
-      </div>
-    );
-  }
+function App() {
+  //define state
+  const [count, setCount] = useState(0);
+  const [item, setItem] = useState(10);
+
+//useMomo use callback function
+const multiCountMemo = useMemo(
+   //define function
+   function multiCount(){
+    console.log("Multi count");
+    return count * 5;
+  }, [count]
+
+)
+
+console.log("Render App Component");
+
+  return (
+    <div>
+      <h4>Count : {count}</h4>
+      <h4>Item : {item}</h4>
+      <h4>{multiCountMemo}</h4>
+      <button onClick={() => setCount(count + 1)}>Update Count</button>
+      <button onClick={() => setItem(item * 10)}>Update Item</button>
+    </div>
+  );
 }
 export default App;
