1ec2cde68073bb06494d97fb0a967077f7ee4dcd
vijay4211 <vijayppandit2042.com>
2024-06-18 00:12:48 +0530

    useLocation

--- src/component/About.js
+++ src/component/About.js
@@ -6,7 +6,7 @@ function About() {
       <p>This is About Page of our awsome App</p>
       <p>And here we are learning About Router</p>
       <Link to="/">Go to Home Page</Link>
-      <li><Link to={"/user/Anil"}>Anil</Link></li>
+      <li><Link to={"/user/Anil"} state={{name : "Anil Siddhu", age:27}}>Anil</Link></li>
       <li><Link to={"/user/Peter"}>Peter</Link></li>
       <li><Link to={"/user/John"}>John</Link></li>
     </div>
--- src/component/User.js
+++ src/component/User.js
@@ -1,9 +1,11 @@
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 
 function User(){
     const params = useParams();
     const {name} = params;
     console.log(name);
+    const location = useLocation();
+    console.log(location);
     return(
         <div>
             <h1>This is {name}'s Page</h1>
