18aeb194cafe0bc2b7aa78bedf421ceca7e147da
vijay4211 <vijayppandit2042.com>
2024-06-17 23:22:56 +0530

    useNavigator

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,11 +1,18 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 function Home() {
+
+  const navigate = useNavigate();
+  const navToPage = (url)=>{
+    navigate(url);
+  }
   return (
     <div>
       <h3>Home Page</h3>
       <p>This is Home Page of our awsome App</p>
       <p>And here we are learning about Router</p>
-      <Link to="/about">Go to About Page</Link>
+      <Link to="/about">Go to About Page</Link><br /><br />
+      <button onClick={()=> navToPage("/about")}>Go to About Page</button><br />
+      <button onClick={()=> navToPage("/filter")}>Go to Filter Page</button><br />
     </div>
   );
 }
